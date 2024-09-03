0d15aae5b80f7625d0d65bd235b42543b8d5832e
sarvesh rajpure <rajpuresarvesh@gmail.com>
2024-09-03 19:49:16 +0530

    Initial CRA commit

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+
+const Home = () => {
+  return (
+    <div className="bg-red-500 h-60">
+      Home
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
